ad6b0547c8b3182ecf0070b44d83063202769a41
Gwen <g@10degres.net>
2017-03-14 12:51:07 +0200

    web/

---
+++ web/js/main.js
@@ -0,0 +1,217 @@
+ $(document).ready(function () {
+	 
+ 	$(".ts-sidebar-menu li a").each(function () {
+ 		if ($(this).next().length > 0) {
+ 			$(this).addClass("parent");
+ 		};
+ 	})
+ 	var menux = $('.ts-sidebar-menu li a.parent');
+ 	$('<div class="more"><i class="fa fa-angle-down"></i></div>').insertBefore(menux);
+ 	$('.more').click(function () {
+ 		$(this).parent('li').toggleClass('open');
+ 	});
+	$('.parent').click(function (e) {
+		e.preventDefault();
+ 		$(this).parent('li').toggleClass('open');
+ 	});
+ 	$('.menu-btn').click(function () {
+ 		$('nav.ts-sidebar').toggleClass('menu-open');
+ 	});
+	 
+	var dtSettingsDefault = {
+		'aaSorting': [],
+		'paging': false,
+		"searching": false,
+		"info": false
+	};
+	
+	 $('#zctb').DataTable( dtSettingsDefault );
+	 
+	 $("#input-43").fileinput({
+		showPreview: false,
+		allowedFileExtensions: ["zip", "rar", "gz", "tgz"],
+		elErrorContainer: "#errorBlock43"
+			// you can configure `msgErrorClass` and `msgInvalidFileExtension` as well
+	});
+
+	 setTimeout("$('.alert-dismissible').fadeOut('slow')", 3000)
+ });
+
+ 
+ function reloadBlock( block_selector, url, method, delay )
+ {
+	 if( method === undefined || method === null ) {
+		 method = 'GET';
+ 	 }
+	 
+	 block = $(block_selector);
+	 if( !block.length ) {
+		 return false;
+	 }
+
+	 wls = window.location.search;
+	 if( url.indexOf('?') >= 0 ) {
+		 wls = wls.replace( '?', '&' );
+	 }
+	 url_to_call = url + wls;
+	 //alert( url_to_call );
+
+	 $.ajax({
+		 url: url,
+		 method: method
+	 }).done(function(data){
+		 if( data == 'STOP' ) {
+			 return false;
+		 } else {
+			 block.replaceWith( data );
+			 if( delay != undefined ) {
+				 timeouts.push( window.setTimeout( "reloadBlock('"+block_selector+"','"+url+"','"+method+"',"+delay+")", parseInt(delay)*1000 ) );
+			 }
+		 }
+	 });
+
+	 return true;
+ }
+ 
+ 
+ /**
+  *
+  *  Base64 encode / decode
+  *  http://www.webtoolkit.info/
+  *
+  **/
+ var Base64 = {
+
+// private property
+	 _keyStr: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
+
+// public method for encoding
+	 encode: function (input) {
+		 var output = "";
+		 var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
+		 var i = 0;
+
+		 input = Base64._utf8_encode(input);
+
+		 while (i < input.length) {
+
+			 chr1 = input.charCodeAt(i++);
+			 chr2 = input.charCodeAt(i++);
+			 chr3 = input.charCodeAt(i++);
+
+			 enc1 = chr1 >> 2;
+			 enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+			 enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+			 enc4 = chr3 & 63;
+
+			 if (isNaN(chr2)) {
+				 enc3 = enc4 = 64;
+			 } else if (isNaN(chr3)) {
+				 enc4 = 64;
+			 }
+
+			 output = output +
+				 this._keyStr.charAt(enc1) + this._keyStr.charAt(enc2) +
+				 this._keyStr.charAt(enc3) + this._keyStr.charAt(enc4);
+
+		 }
+
+		 return output;
+	 },
+
+// public method for decoding
+	 decode: function (input) {
+		 var output = "";
+		 var chr1, chr2, chr3;
+		 var enc1, enc2, enc3, enc4;
+		 var i = 0;
+
+		 input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+
+		 while (i < input.length) {
+
+			 enc1 = this._keyStr.indexOf(input.charAt(i++));
+			 enc2 = this._keyStr.indexOf(input.charAt(i++));
+			 enc3 = this._keyStr.indexOf(input.charAt(i++));
+			 enc4 = this._keyStr.indexOf(input.charAt(i++));
+
+			 chr1 = (enc1 << 2) | (enc2 >> 4);
+			 chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+			 chr3 = ((enc3 & 3) << 6) | enc4;
+
+			 output = output + String.fromCharCode(chr1);
+
+			 if (enc3 != 64) {
+				 output = output + String.fromCharCode(chr2);
+			 }
+			 if (enc4 != 64) {
+				 output = output + String.fromCharCode(chr3);
+			 }
+
+		 }
+
+		 output = Base64._utf8_decode(output);
+
+		 return output;
+
+	 },
+
+// private method for UTF-8 encoding
+	 _utf8_encode: function (string) {
+		 string = string.replace(/\r\n/g, "\n");
+		 var utftext = "";
+
+		 for (var n = 0; n < string.length; n++) {
+
+			 var c = string.charCodeAt(n);
+
+			 if (c < 128) {
+				 utftext += String.fromCharCode(c);
+			 }
+			 else if ((c > 127) && (c < 2048)) {
+				 utftext += String.fromCharCode((c >> 6) | 192);
+				 utftext += String.fromCharCode((c & 63) | 128);
+			 }
+			 else {
+				 utftext += String.fromCharCode((c >> 12) | 224);
+				 utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+				 utftext += String.fromCharCode((c & 63) | 128);
+			 }
+
+		 }
+
+		 return utftext;
+	 },
+
+// private method for UTF-8 decoding
+	 _utf8_decode: function (utftext) {
+		 var string = "";
+		 var i = 0;
+		 var c = c1 = c2 = 0;
+
+		 while (i < utftext.length) {
+
+			 c = utftext.charCodeAt(i);
+
+			 if (c < 128) {
+				 string += String.fromCharCode(c);
+				 i++;
+			 }
+			 else if ((c > 191) && (c < 224)) {
+				 c2 = utftext.charCodeAt(i + 1);
+				 string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
+				 i += 2;
+			 }
+			 else {
+				 c2 = utftext.charCodeAt(i + 1);
+				 c3 = utftext.charCodeAt(i + 2);
+				 string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+				 i += 3;
+			 }
+
+		 }
+
+		 return string;
+	 }
+ }
+ 
